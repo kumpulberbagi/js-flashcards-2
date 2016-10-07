fda122858cc7b6bacbe3c4fceff3dbe485c0c284
Ahyana Rizky Pratama & Aji Lantang Mardika <pair+ahyanarizky.ajilantang@devbootcamp.com>
2016-10-07 17:04:40 +0700

    js flashcard 2

--- controller.js
+++ controller.js
@@ -1,2 +1,43 @@
 "use strict"
 // write your code here
+var fs =require ('fs')
+var contents = fs.readFileSync("social.json");
+var data = JSON.parse(contents);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+class Game{
+  constructor(){
+    this.data = data
+    this.numSoal = 0;
+    this.count = 0;
+  }
+
+  driver(){
+    if(this.numSoal === this.data.length){
+      console.log(`anda menjawab sebanyak :${this.count}`);
+      console.log(`Quiz selesai`);
+        rl.close();
+      }else{
+    rl.question(`${this.data[this.numSoal].definition}\n`, (input) => {
+
+        if(input.toLowerCase() === this.data[this.numSoal].term.toLowerCase()){
+          console.log(`Jawaban benar`);
+          this.numSoal += 1
+          this.count +=1
+          this.driver()
+          }else{
+            console.log(`Jawaban Salah`);
+            this.driver()
+            this.count+=1
+          }
+        });
+      }
+  }
+}
+
+var tanya = new Game()
+tanya.driver()
--- model.js
+++ model.js
@@ -1,2 +1,53 @@
 "use strict"
 // write your code here
+var fs =require ('fs')
+var contents = fs.readFileSync("social.json");
+var data = JSON.parse(contents);
+
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function writeJson(){
+  var write = fs.writeFileSync("data.json", JSON.stringify(data));
+}
+//
+class Question {
+  constructor(quest) {
+    this.quest = quest;
+  }
+
+  addQuest(quest){
+    var input = data[data.length-1].definition;
+    input = quest;
+    data.push({"definition": input, "term": "asaal"});
+    writeJson()
+  }
+
+  addAnswer(answer){
+    data[data.length-1].term = answer
+    writeJson()
+  }
+  jalankan (){
+    rl.question(`masukkan pertanyaan\n`, (tanya) => {
+      this.addQuest(tanya);
+        rl.question(`masukkan jawaban\n`, (jawab) => {
+          this.addAnswer(jawab);
+          rl.question(`input lagi?(y/n)\n`, (lanjut) => {
+            if (lanjut === "n") {
+              rl.close()
+            }else {
+              this.jalankan()
+            }
+            });
+        });
+    });
+  }
+}
+var test = new Question();
+
+test.jalankan()
