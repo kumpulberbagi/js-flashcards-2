a791db311c51bc2c7888be0b319761b4a11bdbb1
Dharmadi Tanamas & Elkana Lumbantobing <pair+dharmadi93.elkana-ltobing@hacktiv8.com>
2016-10-14 10:15:38 +0700

    flashcard 2 update

--- controller.js
+++ controller.js
@@ -1,11 +1,21 @@
 "use strict"
 
 // write your code here
-var view = require("./view.js")
+import View from './view.js'
 var model = require("./model.js")
 var jsonfile = require('jsonfile')
 var file = ''
 var argv = process.argv
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+var tebakan = 0
+var result = []
 
 argv.forEach((val, index) => {
   file = val
@@ -16,7 +26,63 @@ jsonfile.readFile(file, function(err, obj) {
     console.log("file tidak ada");
   }
   else {
-    view.getView(model.getData(file))
+    var data = model.getData(file)
+    View.welcome()
+    let soal = (nomor,index,stats) => {
+      result[nomor] = tebakan
+      if(nomor<index) {
+        if(stats == 0) {
+          tebakan = 0
+          console.log(data[nomor].definition);
+        }
+
+        rl.question("Guess: ", (answer) => {
+
+          while (answer != data[nomor].term.toLowerCase()) {
+            console.log("Incorrect! try again.");
+            tebakan +=1
+            return soal(nomor, index, 1)
+          }
+
+          tebakan +=1
+          result[nomor] = tebakan
+
+          if(nomor>=index-1) {
+            var maxVal = 0
+            var same = 0
+            var total = 0
+            for (var i = 0; i < result.length; i++) {
+              if(maxVal < result[i]) {
+                maxVal = result[i]
+              }
+              else {
+                maxVal = maxVal
+              }
+
+              if (result[0] == result[i]) {
+                same++
+              }
+            }
+
+            if (same != data.length) {
+              for (var i = 0; i < result.length; i++) {          
+                if(result[i] == maxVal) {
+                  console.log(`Soal yang paling sulit di tebak ada lah:`);
+                  console.log(data[i].definition);
+                }
+                total += result[i]
+              }
+            }
+
+            console.log(`Total tebakan anda adalah ${total}`);
+            rl.close()
+          }
+
+          return soal(nomor+1, index,0)
+        });
+      }
+    }
+    soal(0,data.length,0)
   }
 
 })
--- view.js
+++ view.js
@@ -2,40 +2,15 @@
 // write your code here
 // var model = require("./model.js")
 
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+class View {
+  constructor() {
 
-var tebakan = 0
+  }
 
-module.exports = {
-  getView: function (data) {
-    let soal = (nomor,index,stats) => {
-      if(nomor<index) {
-        if(stats == 0) {
-          console.log(data[nomor].definition);
-        }
-
-        rl.question("Guess: ", (answer) => {
-          while (answer != data[nomor].term.toLowerCase()) {
-            console.log("Incorrect! try again.");
-            tebakan +=1
-            return soal(nomor, index, 1)
-          }
-
-          if(nomor>=index-1) {
-            console.log(`Total tebakan anda: ${tebakan}`);
-            rl.close()
-          }
-          tebakan +=1
-          return soal(nomor+1, index,0)
-        });
-      }
-    }
+  static welcome () {
     console.log("Welcome to JS Flash Cards. To play, just enter the corret term for each definition. Ready? Go!");
-    soal(0,data.length,0)
   }
 }
+
+export default View
