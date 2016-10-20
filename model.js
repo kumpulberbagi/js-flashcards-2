455b84060cfbdfcaf0aa0e6ab0ee94c7e94d374a
Alessandro <Alessandro.christopher@gmail.com>
2016-10-20 15:27:12 +0700

    Submitted

--- model.js
+++ model.js
@@ -1,2 +1,18 @@
 "use strict"
 // write your code here
+module.exports = {
+
+  getData: function(json){
+    const fs = require('fs')
+    var data = fs.readFileSync(json)
+    data = JSON.parse(data)
+    return data
+
+  },
+  answer: function(data, count){
+    return data[count].term
+  },
+  questions: function(data,count) {
+     return data[count].definition;
+  }
+}
--- view.js
+++ view.js
@@ -1,2 +1,19 @@
 "use strict"
 // write your code here
+
+// function questions(data){
+//   console.log("------",data);
+//   // console.log(x[1].definition + "\n");
+// }
+
+var model = require('./model.js')
+module.exports = {
+  display: function(){
+    console.log("Welcome to js-flash, which deck would you like to use today?\n");
+  },
+  questions: function(data, count){
+    console.log(model.questions(data, count));
+  }
+
+
+}
